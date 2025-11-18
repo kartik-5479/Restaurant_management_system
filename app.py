9fc68d27f32f074c4ff42a06b5a4f67e683bcb60
kartik-5479 <kartik1101abc@gmail.com>
2025-11-18 22:17:25 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for # pyright: ignore[reportMissingImports]
 import random, time
 from difflib import get_close_matches
 from datetime import datetime
